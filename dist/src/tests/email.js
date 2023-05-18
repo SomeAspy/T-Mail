3db4f096445e52af63bc9aaf471b254731d81458
Aiden <amb@aspy.dev>
2023-05-17 22:01:07 -0400

    fix gitignore and linting

--- dist/src/tests/email.js
+++
@@ -1,6 +0,0 @@
-import { sendEmail } from '../sendEmail.js';
-console.log('Starting Email test...');
-sendEmail(24, 33).then((result) => {
-    console.log(`Email test result: ${result ? 'passed' : 'failed'}!`);
-    process.exit(0);
-});
