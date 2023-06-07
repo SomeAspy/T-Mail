2672d0e51fd3c8c6cfa1ca8adc620ff1dd688cb2
Aiden <amb@aspy.dev>
2023-06-07 11:22:39 -0400

    exact file paths /root/

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -3,23 +3,23 @@
 # OPTIONALLY USE A CRON SCRIPT TO RUN ON AN INTERVAL
 
 # get the lastest build and unzip
-wget -O dist.zip https://nightly.link/SomeAspy/T-Mail/workflows/build/main/dist.zip
-unzip -o dist.zip -d dist
+wget -O /root/dist.zip https://nightly.link/SomeAspy/T-Mail/workflows/build/main/dist.zip
+unzip -o /root/dist.zip -d /root/dist
 
 # update this script
-mv dist/scripts/dist.sh dist.sh
+mv /root/dist/scripts/dist.sh /root/dist.sh
 
 # Enter working directory
-cd dist
+cd rootdist
 
 # update/install dependencies (Twice as raspi0x32 is a bit buggy)
 pnpm i -P
 pnpm i -P
 
 # restart / start PM2 if it isn't running already
-cd ..
-pm2 restart pm2.config.cjs || pm2 start pm2.config.cjs
+pm2 restart /root/dist/pm2.config.cjs || pm2 start /root/dist/pm2.config.cjs
 pm2 save
 
 # cleanup
-rm dist.zip
+cd ..
+rm /root/dist.zip
